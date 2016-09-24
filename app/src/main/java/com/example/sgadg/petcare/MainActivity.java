4df39b2ad7bdf7d2dd61f33a77ba67ee1c4c5fd5
Soham Gadgil <sohamudaygadgil@gmail.com>
2016-09-24 18:16:55 -0400

    Latest Commit

--- app/src/main/java/com/example/sgadg/petcare/MainActivity.java
+++ app/src/main/java/com/example/sgadg/petcare/MainActivity.java
@@ -1,39 +1,127 @@
 package com.example.sgadg.petcare;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mTextDetails;
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
+        facebookSDKInitialize();
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mTextDetails = (TextView) findViewById(R.id.text_details);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        getLoginDetails(loginButton);
+//        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    }
+
+    protected void getLoginDetails(LoginButton login_button) {
+        login_button.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken =loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+                if(profile != null) {
+                    mTextDetails.setText("Welcome To Pet Care, " + profile.getName() + "!");
+                    Log.d("abc", profile.getFirstName());
+                    Log.d("bcd", profile.getId());
+                    //startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("fb://messaging/"+profile.getId())));
+
+                }
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
             }
         });
+    }
+
+    protected void facebookSDKInitialize() {
+                FacebookSdk.sdkInitialize(getApplicationContext());
+                mCallbackManager = CallbackManager.Factory.create();
+    }
+//   @Override
+//    public void onViewCreated(View view, Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//        LoginButton loginButton = (LoginButton) view.findViewById(R.id.login_button);
+//        loginButton.setReadPermissions("email");
+//        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+//            @Override
+//            public void onSuccess(LoginResult loginResult) {
+//                AccessToken accessToken =loginResult.getAccessToken();
+//                Profile profile = Profile.getCurrentProfile();
+//                if(profile != null) {
+//                    mTextDetails.setText("Welcome " + profile.getName());
+//                }
+//            }
+//
+//            @Override
+//            public void onCancel() {
+//
+//            }
+//
+//            @Override
+//            public void onError(FacebookException error) {
+//
+//            }
+//        };
+//
+//    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AppEventsLogger.deactivateApp(this);
     }
 
     @Override
@@ -42,6 +130,14 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        Log.e("data", data.toString());
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
