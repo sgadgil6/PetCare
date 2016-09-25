5b6132635a368fa7232cd769ecb6cbdc861e58a9
Sean Sain <seanrsain@gmail.com>
2016-09-24 23:02:43 -0400

    Made User and Host GUI

--- app/src/main/java/com/example/sgadg/petcare/HostSignUp.java
+++ app/src/main/java/com/example/sgadg/petcare/HostSignUp.java
@@ -1,6 +1,7 @@
 package com.example.sgadg.petcare;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,14 +10,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
-public class UserSignUp extends AppCompatActivity {
-
-
+public class HostSignUp extends AppCompatActivity {
+    String fname;
+    String lname;
+    String id;
+    String profPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_sign_up);
+        setContentView(R.layout.activity_host_sign_up);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,14 +31,14 @@ public class UserSignUp extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        Intent userIntent = getIntent();
-        String fname = userIntent.getStringExtra("fname");
-        String lname = userIntent.getStringExtra("lname");
-        String id = userIntent.getStringExtra("id");
-        String profPic = userIntent.getStringExtra("profPic");
-        TextView t = (TextView)findViewById(R.id.user_welcome);
+        Intent hostIntent = getIntent();
+        fname = hostIntent.getStringExtra("fname");
+        lname = hostIntent.getStringExtra("lname");
+        id = hostIntent.getStringExtra("id");
+        profPic = hostIntent.getStringExtra("profPic");
+        TextView t = (TextView)findViewById(R.id.host_welcome);
         t.setText("Hi " + fname + ". Please enter your info.");
-
+        Uri profPicUri = Uri.parse(profPic);
     }
 
 }
